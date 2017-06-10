52be4744e3515f4fde3148d3f16a9949d9730b08
javinor <javinor@gmail.com>
2017-06-10 19:48:43 +0300

    pass recursive function as an argument

--- factorial.js
+++ factorial.js
@@ -5,4 +5,12 @@ const f1 = (n) => n < 2 ? 1 : n * f1(n - 1)
 factorials.push(f1)
 
 
+// pass in the recursive function as an argument
+const f2 = (f, n) => {
+  return n < 2 ? 1 : n * f(f, n - 1)
+}
+factorials.push(n => f2(f2, n))
+
+
+
 module.exports = {factorials}
